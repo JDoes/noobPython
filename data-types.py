9c7963d5e06d676e056afde1f95383037c1d6309
Gordon <47766574+JDoes@users.noreply.github.com>
2020-10-29 13:45:37 -0700

    Create data-types.py
    compared numbers as a string type, versus numbers as int (or float) type
    Change the numbers on lines 1 and 2 to see the affect.

---
+++ data-types.py
@@ -0,0 +1,15 @@
+Num1 = 10
+Num2 = "10"
+
+x = Num1*5 
+y = Num2*5 
+
+print("Num1 as an integer: " + str(Num1))
+print("Num2 as a string: " + "\"" + Num2 + "\"")
+
+print("\nThe variable Num1 = " + str(Num1) + " is of the type: " + str(type(Num1)))
+print("The variable Num2 = \"" + Num2 + "\" is of the type: " + str(type(Num2)))
+
+print("")
+print("Using " + str(Num1) + " as an integer number: " + str(Num1) + "*5 = " + str(x))
+print("Using " + Num2 + " as a string value: " + str(Num2) + "*5 = " + y)
